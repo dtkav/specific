7a2ced995b8bf59f792ba0eeb2fa634d071da109
Daniel Grossmann-Kavanagh <me@danielgk.com>
2018-11-07 15:48:08 -0800

    redo validation by content type

--- connexion/decorators/validation.py
+++ connexion/decorators/validation.py
@@ -9,67 +9,13 @@ from jsonschema import Draft4Validator, ValidationError, draft4_format_checker
 from werkzeug import FileStorage
 
 from ..exceptions import ExtraParameterProblem
-from ..http_facts import FORM_CONTENT_TYPES
 from ..json_schema import Draft4RequestValidator, Draft4ResponseValidator
 from ..problem import problem
-from ..utils import all_json, boolean, is_json_mimetype, is_null, is_nullable
+from ..serialization.deserializers import DEFAULT_DESERIALIZERS
+from ..types import TypeValidationError, coerce_type
+from ..utils import is_null, is_nullable
 
 logger = logging.getLogger('connexion.decorators.validation')
-
-TYPE_MAP = {
-    'integer': int,
-    'number': float,
-    'boolean': boolean
-}
-
-
-class TypeValidationError(Exception):
-    def __init__(self, schema_type, parameter_type, parameter_name):
-        """
-        Exception raise when type validation fails
-
-        :type schema_type: str
-        :type parameter_type: str
-        :type parameter_name: str
-        :return:
-        """
-        self.schema_type = schema_type
-        self.parameter_type = parameter_type
-        self.parameter_name = parameter_name
-
-    def __str__(self):
-        msg = "Wrong type, expected '{schema_type}' for {parameter_type} parameter '{parameter_name}'"
-        return msg.format(**vars(self))
-
-
-def coerce_type(param, value, parameter_type, parameter_name=None):
-
-    def make_type(value, type_literal):
-        type_func = TYPE_MAP.get(type_literal)
-        return type_func(value)
-
-    param_schema = param.get("schema", param)
-    if is_nullable(param_schema) and is_null(value):
-        return None
-
-    param_type = param_schema.get('type')
-    parameter_name = parameter_name if parameter_name else param.get('name')
-    if param_type == "array":
-        converted_params = []
-        for v in value:
-            try:
-                converted = make_type(v, param_schema["items"]["type"])
-            except (ValueError, TypeError):
-                converted = v
-            converted_params.append(converted)
-        return converted_params
-    else:
-        try:
-            return make_type(value, param_type)
-        except ValueError:
-            raise TypeValidationError(param_type, parameter_type, parameter_name)
-        except TypeError:
-            return value
 
 
 def validate_parameter_list(request_params, spec_params):
@@ -100,11 +46,25 @@ class RequestBodyValidator(object):
         self.validator = validatorClass(schema, format_checker=draft4_format_checker)
         self.api = api
         self.strict_validation = strict_validation
+        self._content_handlers = [
+            de(self.validator,
+               self.schema,
+               self.strict_validation,
+               self.is_null_value_valid) for de in DEFAULT_DESERIALIZERS
+        ]
 
-    def validate_formdata_parameter_list(self, request):
-        request_params = request.form.keys()
-        spec_params = self.schema.get('properties', {}).keys()
-        return validate_parameter_list(request_params, spec_params)
+    def register_content_handler(self, cv):
+        self._content_handlers += [cv]
+
+    def lookup_content_handler(self, request):
+        matches = [
+            v for v in self._content_handlers
+            if v.regex.match(request.content_type)
+        ]
+        if len(matches) > 1:
+            logger.warning("Content could be handled by multiple validators")
+        if matches:
+            return matches[0]
 
     def __call__(self, function):
         """
@@ -114,80 +74,27 @@ class RequestBodyValidator(object):
 
         @functools.wraps(function)
         def wrapper(request):
-            if all_json(self.consumes):
-                data = request.json
+            content_handler = self.lookup_content_handler(request)
+            exact_match = request.content_type in self.consumes
+            partial_match = content_handler and content_handler.name in self.consumes
+            if not (exact_match or partial_match):
+                msg = "Invalid Content-type ({content_type}), expected one of {consumes}"
+                msg = msg.format(content_type=request.content_type, consumes=self.consumes)
+                return problem(415, "Unsupported Media Type", msg)
 
-                empty_body = not(request.body or request.form or request.files)
-                if data is None and not empty_body and not self.is_null_value_valid:
-                    try:
-                        ctype_is_json = is_json_mimetype(request.headers.get("Content-Type", ""))
-                    except ValueError:
-                        ctype_is_json = False
-
-                    if ctype_is_json:
-                        # Content-Type is json but actual body was not parsed
-                        return problem(400,
-                                       "Bad Request",
-                                       "Request body is not valid JSON"
-                                       )
-                    else:
-                        # the body has contents that were not parsed as JSON
-                        return problem(415,
-                                       "Unsupported Media Type",
-                                       "Invalid Content-type ({content_type}), expected JSON data".format(
-                                           content_type=request.headers.get("Content-Type", "")
-                                       ))
-
-                logger.debug("%s validating schema...", request.url)
-                error = self.validate_schema(data, request.url)
-                if error and not self.has_default:
-                    return error
-            elif self.consumes[0] in FORM_CONTENT_TYPES:
-                data = dict(request.form.items()) or (request.body if len(request.body) > 0 else {})
-                data.update(dict.fromkeys(request.files, ''))  # validator expects string..
-                logger.debug('%s validating schema...', request.url)
-
-                if self.strict_validation:
-                    formdata_errors = self.validate_formdata_parameter_list(request)
-                    if formdata_errors:
-                        raise ExtraParameterProblem(formdata_errors, [])
-
-                if data:
-                    props = self.schema.get("properties", {})
-                    errs = []
-                    for k, param_defn in props.items():
-                        if k in data:
-                            try:
-                                data[k] = coerce_type(param_defn, data[k], 'requestBody', k)
-                            except TypeValidationError as e:
-                                errs += [str(e)]
-                                print(errs)
-                    if errs:
-                        return problem(400, 'Bad Request', errs)
-
-                error = self.validate_schema(data, request.url)
+            if content_handler:
+                error = content_handler.validate(request)
                 if error:
                     return error
+
+            elif self.strict_validation:
+                logger.warning("No handler for ({content_type})".format(
+                    content_type=request.content_type))
 
             response = function(request)
             return response
 
         return wrapper
-
-    def validate_schema(self, data, url):
-        # type: (dict, AnyStr) -> Union[ConnexionResponse, None]
-        if self.is_null_value_valid and is_null(data):
-            return None
-
-        try:
-            self.validator.validate(data)
-        except ValidationError as exception:
-            logger.error("{url} validation error: {error}".format(url=url,
-                                                                  error=exception.message),
-                         extra={'validator': 'body'})
-            return problem(400, 'Bad Request', str(exception.message))
-
-        return None
 
 
 class ResponseBodyValidator(object):
